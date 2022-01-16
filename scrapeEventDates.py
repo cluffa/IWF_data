1eebd3ff7f79956808e666a532a1c0cd14c21426
Alex Cluff <alexcluff16@gmail.com>
2022-01-16 14:55:07 -0500

    rename

--- scrapeEventDates.py
+++
@@ -1,2 +0,0 @@
-# https://iwf.sport/results/results-by-events/?event_type=all&event_age=all&event_nation=all
-# https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_type=all&event_age=all&event_nation=all
