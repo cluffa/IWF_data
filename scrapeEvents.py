1ec72b342b92d9fa73ed2cc321b5c75d69fa21af
Alex Cluff <alexcluff16@gmail.com>
2022-01-16 14:54:25 -0500

    scrape events

---
+++ scrapeEvents.py
@@ -0,0 +1,46 @@
+# https://iwf.sport/results/results-by-events/?event_type=all&event_age=all&event_nation=all
+# https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_type=all&event_age=all&event_nation=all
+
+from bs4 import BeautifulSoup
+import bs4
+import requests
+import pandas as pd
+import numpy as np
+combined = pd.DataFrame()
+
+urls = ['https://iwf.sport/results/results-by-events/?event_type=all&event_age=all&event_nation=all',
+    'https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_type=all&event_age=all&event_nation=all']
+
+for url in urls:
+    df = pd.DataFrame()
+    req = requests.get(url)
+    content = req.text
+    soup = BeautifulSoup(content, 'html.parser').find('div', 'cards')
+
+    ids = []
+    for id in soup.find_all('a', 'card', href=True):
+        ids.append(int(id['href'].replace('?event_id=', '')))
+
+    df['id'] = ids
+
+    events = []
+    for event in soup.find_all('span', 'text'):
+        events.append(event.get_text())
+
+    df['event'] = events
+
+    dates = []
+    for date in soup.find_all('div', 'col-md-2 col-4 not__cell__767'):
+        dates.append(date.get_text().strip())
+
+    df['date'] = dates
+
+    locations = []
+    for country in soup.find_all('div', 'col-md-3 col-4 not__cell__767'):
+        locations.append(country.get_text().strip())
+
+    df['location'] = locations
+
+    combined = combined.append(df)
+
+combined.sort_values('id').to_csv('./raw_data/events.csv', index=False)
